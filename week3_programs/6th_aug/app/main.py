25c20f003d8ccb64a07abaa623694dedb1e4c0f8
Eniyan-Ravi <eniyanrio@gmail.com>
2026-08-06 15:28:32 +0530

    file updated

--- week3_programs/6th_aug/app/main.py
+++ week3_programs/6th_aug/app/main.py
@@ -37,3 +37,8 @@ async def read_todo(db: db_dependency, todo_id: int=Path(gt=0)):
         return todo_model 
     raise HTTPException(status_code=404, detail='Todo not found.')
 
+@app.post("/todo",status_code=status.HTTP_201_CREATED)
+async def created_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+    db.add(todo_model)
+    db.commit()
